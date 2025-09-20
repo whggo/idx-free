fd3b4326545d5ffee30c6df1a49bc558458d122c
whggo <31363442+whggo@users.noreply.github.com>
2025-09-20 20:22:22 +0800

    Create install.sh

---
+++ s-box/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -rf .tmp >/dev/null 2>&1
+
+ARCH=$(uname -m)
+case $ARCH in
+    "aarch64" | "arm64" | "arm")
+        ARCH="arm64"
+        ;;
+    "x86_64" | "amd64" | "x86")
+        ARCH="amd64"
+        ;;
+    "s390x" | "s390")
+        ARCH="s390x"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+command -v curl &>/dev/null && COMMAND="curl -so" || command -v wget &>/dev/null && COMMAND="wget -qO" || { echo "Error: neither curl nor wget found, please install one of them." >&2; exit 1; }
+
+$COMMAND sbx "https://$ARCH.ssss.nyc.mn/s-box"
+
+chmod +x sbx && ./sbx
+echo -e "\n\033[1;32m安装完成\033[0m"
+echo -e "\n\033[1;32m一键卸载命令: pkill -f '\.tmp/'\033[0m\n"
