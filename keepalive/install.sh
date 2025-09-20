fcdb7166e52de95be7f339c8b6a21eb4b928c74b
whggo <31363442+whggo@users.noreply.github.com>
2025-09-20 19:42:03 +0800

    Update install.sh

--- keepalive/install.sh
+++ keepalive/install.sh
@@ -9,12 +9,12 @@ mkdir -p app/idx-keepalive
 cd app/idx-keepalive
 
 # 3. download keepalive scripts & install dependencies
-wget https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/google-idx/keepalive/app.js
-wget https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/google-idx/keepalive/package.json
+wget https://raw.githubusercontent.com/whggo/idx-free/refs/heads/main/keepalive/app.js
+wget https://raw.githubusercontent.com/whggo/idx-free/refs/heads/main/keepalive/package.json
 npm install
 
 # 4. create startup.sh
-wget https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/google-idx/keepalive/startup.sh
+wget https://raw.githubusercontent.com/whggo/idx-free/refs/heads/main/keepalive/startup.sh
 sed -i 's#$PWD#'$PWD'#g' startup.sh
 chmod +x startup.sh
 
